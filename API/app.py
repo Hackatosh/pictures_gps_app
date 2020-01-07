ca1dcab65f759480c9f9fd16190b3f2fb60b6c40
TheoD <theo.dolle@student.ecp.fr>
2020-01-07 19:43:36 +0100

    wip front

--- API/app.py
+++ API/app.py
@@ -1,26 +1,29 @@
-from flask import Flask, json, request, jsonify
+from flask import Flask, json, request, jsonify, render_template
 
 LOCATION_STOLEN={"userId1":[0.0, 0.0, 0.0]}
+LOCATION_STOLEN_LIST=["test"]
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("result.html")
 
 @app.route('/newInfoStolen', methods = ['POST'])
 def api_message():
     if request.headers['Content-Type'] == 'application/json':
         content = request.json
         userId = content["userId"]
+        LOCATION_STOLEN_LIST.append(content)
         if userId is not None :
             if userId not in LOCATION_STOLEN.keys() :
                 LOCATION_STOLEN[userId]=[]
             LOCATION_STOLEN[userId].append(content["exifInfos"])
         else:
             print("incorrect Json file")
+        print("new location_stolen : {}".format(LOCATION_STOLEN))
         return "location_stolen : {}".format(LOCATION_STOLEN)
 
 @app.route('/getStolenInfo', methods=['GET'])
 def get_stolen_info():
-    return jsonify(LOCATION_STOLEN)
+    return jsonify({'results': LOCATION_STOLEN_LIST})
